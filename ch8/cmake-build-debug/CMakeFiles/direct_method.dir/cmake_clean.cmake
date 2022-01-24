31f5c0bc92fdde10383e953e35e988d88eca68b6
xiapinquan <xiapinquan@vip.qq.com>
2022-01-24 10:26:37 +0800

    [update ch8 & ch10]

---
+++ ch8/cmake-build-debug/CMakeFiles/direct_method.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/direct_method.dir/direct_method.cpp.o"
+  "direct_method"
+  "direct_method.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/direct_method.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
